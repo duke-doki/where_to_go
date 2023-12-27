eedfc8ad2a8dd002ebe3cc27cd34f12470535c92
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-12-27 17:18:53 +0300

    add pictures

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from places.models import Place, Image
 
@@ -6,6 +7,17 @@ from places.models import Place, Image
 # Register your models here.
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ["pic", ]
+    fields = ("picture", "pic", "number")
+    def pic(model, obj):
+        k = obj.picture.width / obj.picture.height
+        return mark_safe(
+            f'<'
+            f'img src="{obj.picture.url}" '
+            f'width="{k * 200}" '
+            f'height={200} '
+            f'/>'
+        )
 
 
 @admin.register(Place)
@@ -19,3 +31,14 @@ class PlaceAdmin(admin.ModelAdmin):
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_filter = ('place', )
+    readonly_fields = ["pic", ]
+
+    def pic(self, obj):
+        k = obj.picture.width / obj.picture.height
+        return mark_safe(
+            f'<'
+            f'img src="{obj.picture.url}" '
+            f'width="{k * 200}" '
+            f'height={200} '
+            f'/>'
+        )
