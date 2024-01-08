86fcd20e7e55381c2bef314a4008b5974942436b
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-01-08 09:27:38 +0300

    remove migrations

--- places/migrations/0003_image.py
+++ places/migrations/0003_image.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('places', '0002_auto_20231224_1325'),
+        ('places', '0001_initial'),
     ]
 
     operations = [
--- places/migrations/0008_remove_image_url.py
+++ places/migrations/0008_remove_image_url.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('places', '0007_auto_20231226_1024'),
+        ('places', '0006_image_url'),
     ]
 
     operations = [
