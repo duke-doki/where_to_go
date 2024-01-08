d26a74d8dccbd3efbc9c623a73ae193d7b5412dc
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-01-08 19:24:29 +0300

    add unique

--- places/models.py
+++ places/models.py
@@ -5,7 +5,8 @@ from tinymce.models import HTMLField
 class Place(models.Model):
     title = models.CharField(
         'Место',
-        max_length=100
+        max_length=100,
+        unique=True
     )
     short_description = models.TextField(
         'Короткое описание',
